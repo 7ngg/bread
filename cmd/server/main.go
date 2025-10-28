a5e75316f7d76baf608e02326d8358f4cf6e92e8
7ngg <m.farm4nov@gmail.com>
2025-10-28 14:25:26 +0400

    feat: application config using cleanenv

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,16 +1,43 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
+	"io"
+	"log"
 	"net/http"
 
+	"github.com/7ngg/bread/internal/config"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	_ "github.com/mattn/go-sqlite3"
 )
 
+type Templates struct {
+	templates *template.Template
+}
+
+func (t *Templates) Render(w io.Writer, name string, data any) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func NewTemplates() *Templates {
+	return &Templates{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+}
+
 func main() {
-	chi := chi.NewRouter()
+	cfg := config.MustLoad()
 
-	chi.Use(middleware.Logger)
+	r := chi.NewRouter()
 
-	http.ListenAndServe(":42069", chi)
+	r.Use(middleware.Logger)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		templates := NewTemplates()
+		templates.Render(w, "index", cfg)
+	})
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), r))
 }
---
+++ internal/config/config.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"flag"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type AppConfig struct {
+	Environment string `yaml:"environment" env:"ENVIRONMENT" env-required:"true"`
+	Port        int    `yaml:"port" env:"PORT" env-default:"42069"`
+	DbPath      string `yaml:"db_path" env:"DB_PATH" env-default:"db.sqlite"`
+}
+
+func MustLoad() *AppConfig {
+	path := fetchConfigPath()
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		panic("Config file does not exist at path: " + path)
+	} else if err != nil {
+		panic("Error checking config file: " + err.Error())
+	}
+
+	var cfg AppConfig
+	cleanenv.ReadConfig(path, &cfg)
+
+	return &cfg
+}
+
+func fetchConfigPath() string {
+	var path string
+
+	flag.StringVar(&path, "config", "", "Path to config file")
+	flag.Parse()
+
+	if path == "" {
+		path = os.Getenv("CONFIG_PATH")
+	}
+
+	if path == "" {
+		panic("Config path must be provided via --config flag or CONFIG_PATH environment variable")
+	}
+
+	return path
+}
