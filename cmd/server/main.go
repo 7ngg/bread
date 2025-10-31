2c9186035e6857bdba7a7df487f85f8846c74caf
7ngg <m.farm4nov@gmail.com>
2025-10-31 15:28:54 +0400

    feat: simple navbar

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -31,5 +31,37 @@ func main() {
 
 	r := webapi.NewRouter(cfg)
 
+	fs := http.FileServer(http.Dir("static"))
+	r.Handle("/static/*", http.StripPrefix("/static/", fs))
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		templates := NewTemplates()
+		opts := struct {
+			Items []struct {
+				Href string
+				Text string
+			}
+		}{
+			Items: []struct {
+				Href string
+				Text string
+			}{
+				{
+					Href: "/",
+					Text: "Main",
+				},
+				{
+					Href: "/",
+					Text: "Catalog",
+				},
+			},
+		}
+		err := templates.Render(w, "index.html", opts)
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+	})
+
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), r))
 }
